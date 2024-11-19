74028d3c364c4815d916319070c954b78e786b2b
Larvact <tobyg7794@hotmail.com>
2024-11-19 11:52:47 +0000

    Initial commit for the project created via spring boot initializer

---
+++ src/main/java/com/toby/exchangerate/gateway/ExchangeRateGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.toby.exchangerate.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateGatewayApplication.class, args);
+	}
+
+}
