1c51c8a476196880d312c6c2f863703600fed3f5
Larvact <tobyg7794@hotmail.com>
2024-11-19 18:56:36 +0000

    Added new pom changes based on a new microservice bom

--- src/main/java/toby/exchangerate/gateway/ExchangeRateGatewayApplication.java
+++ src/main/java/toby/exchangerate/gateway/ExchangeRateGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.toby.exchangerate.gateway;
+package toby.exchangerate.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExchangeRateGatewayApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ExchangeRateGatewayApplication.class, args);
 	}
 
