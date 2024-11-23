8b28f50e38a74bd5610dd76fac07021c29317b01
Larvact <tobyg7794@hotmail.com>
2024-11-23 16:28:57 +0000

    Added basic api call to local endpoints to retrieve a response for latest currency exchagne rate

--- src/main/java/toby/exchangerate/gateway/service/exchangeratesapi/ExchangeRatesApiService.java
+++ src/main/java/toby/exchangerate/gateway/service/exchangeratesapi/ExchangeRatesApiService.java
@@ -1,18 +1,28 @@
 package toby.exchangerate.gateway.service.exchangeratesapi;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import toby.exchangerate.gateway.service.ExchangeRatesService;
 import toby.exchangerate.json.api.exchangerates.latest.LatestCurrencyExchangeRatesRequest;
 import toby.exchangerate.json.api.exchangerates.latest.LatestCurrencyExchangeRatesResponse;
 
 @Service
 @Qualifier("exchangeRatesApiService")
+@RequiredArgsConstructor
 public class ExchangeRatesApiService implements ExchangeRatesService
 {
+    private static final String LATEST_CURRENCY_EXCHANGE_RATE_BASE_PATH = "/v1/latest?base=%s&symbols=%s";
+    private final WebClient webClient;
+
     @Override
     public LatestCurrencyExchangeRatesResponse getLatestCurrencyExchangeRate(final LatestCurrencyExchangeRatesRequest latestCurrencyExchangeRatesRequest)
     {
-        return null;
+       return webClient.get()
+               .uri(String.format(LATEST_CURRENCY_EXCHANGE_RATE_BASE_PATH, latestCurrencyExchangeRatesRequest.getBaseCurrencySymbol(), latestCurrencyExchangeRatesRequest.getResponseCurrencySymbols()))
+               .retrieve()
+               .bodyToMono(LatestCurrencyExchangeRatesResponse.class)
+               .block();
     }
 }
