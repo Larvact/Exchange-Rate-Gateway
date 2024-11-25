7e541a0f992c51e6bbe045d5551228ac0cdb4289
Larvact <tobyg7794@hotmail.com>
2024-11-25 19:43:30 +0000

    Added basic framework to get the api key for exchange rates api requests

--- gateway-microservice/src/main/java/toby/exchangerate/gateway/service/exchangeratesapi/ExchangeRatesApiService.java
+++ gateway-microservice/src/main/java/toby/exchangerate/gateway/service/exchangeratesapi/ExchangeRatesApiService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import toby.exchangerate.gateway.configuration.api.ApiKeyRetriever;
+import toby.exchangerate.gateway.configuration.api.exchangerates.ExchangeRatesApiKeyRetriever;
 import toby.exchangerate.gateway.service.ExchangeRatesService;
 import toby.exchangerate.json.api.exchangerates.ExchangeRatesLatestResponse;
 import toby.exchangerate.json.api.exchangerates.error.ErrorDetail;
@@ -17,15 +19,16 @@ import toby.exchangerate.json.api.exchangerates.latest.LatestCurrencyExchangeRat
 @RequiredArgsConstructor
 public class ExchangeRatesApiService implements ExchangeRatesService
 {
-    private static final String LATEST_CURRENCY_EXCHANGE_RATE_BASE_PATH = "/v1/latest?base=%s&symbols=%s";
+    private static final String LATEST_CURRENCY_EXCHANGE_RATE_BASE_PATH = "/v1/latest?access_key=%s&base=%s&symbols=%s";
 
     private final WebClient webClient;
+    private final ApiKeyRetriever apiKeyRetriever;
 
     @Override
     public LatestCurrencyExchangeRatesResponse getLatestCurrencyExchangeRate(final LatestCurrencyExchangeRatesRequest latestCurrencyExchangeRatesRequest)
     {
         return webClient.get()
-                .uri(String.format(LATEST_CURRENCY_EXCHANGE_RATE_BASE_PATH, latestCurrencyExchangeRatesRequest.getBaseCurrencySymbol(), String.join(",", latestCurrencyExchangeRatesRequest.getResponseCurrencySymbols())))
+                .uri(String.format(LATEST_CURRENCY_EXCHANGE_RATE_BASE_PATH, apiKeyRetriever.getApiKey(),latestCurrencyExchangeRatesRequest.getBaseCurrencySymbol(), String.join(",", latestCurrencyExchangeRatesRequest.getResponseCurrencySymbols())))
                 .retrieve()
                 .onStatus(httpStatusCode -> httpStatusCode != HttpStatus.OK, response -> response.bodyToMono(ExchangeRatesErrorResponse.class)
                         .map(ExchangeRatesErrorResponse::getErrorDetail)
